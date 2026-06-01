55e66fd9525416e49968c603bb768af122329042
Jael Lena <jael@bitseconomy.com>
2026-06-01 07:46:36 -0500

    git commit -m "Add React structure for Vercel deployment"

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RCMAssessment from './rcm_assessment_system';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RCMAssessment />
+    </div>
+  );
+}
+
+export default App;
